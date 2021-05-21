f9ef477b8fbf7d5037a4b3bc8f7e86c8d1c8dc3e
Alice Chin <alice.chin@eztable.com>
2021-05-21 15:43:47 +0800

    feat: add config

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const db = require('./models')
 
 app.listen(PORT, () => {
+  db.sequelize.sync()
   console.log(`App is running on http://localhost:${PORT}`)
 })
