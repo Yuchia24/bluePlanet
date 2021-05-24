4b7b4b72b25227ac7f63f06a220abf0bb646effc
Alice Chin <alice.chin@eztable.com>
2021-05-24 13:32:05 +0800

    feat: add cors

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
 const db = require('./models')
+const cors = require('cors')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const PORT = process.env.PORT || 3000
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 app.listen(PORT, () => {
   db.sequelize.sync()
