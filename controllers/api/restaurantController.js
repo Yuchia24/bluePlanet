38d272db738fc2a3b431150e30be767ae81a58fd
Alice Chin <alice.chin@eztable.com>
2021-05-25 15:08:56 +0800

    refactor: wrap all vender actions as functions

--- controllers/api/restaurantController.js
+++ controllers/api/restaurantController.js
@@ -1,23 +1,69 @@
 const apiHelper = require('../../utils/helper')
-const { data1, vender, vender_restaurant_keyword_rawData, vender_input_data } = require('../../models')
+// const { data1, vender, vender_restaurant_keyword_rawData, vender_input_data } = require('../../models')
 const token = process.env.token
 const qs = require('qs')
 
+async function getVenderKeyword (req, res) {
+  try {
+    const { kw } = req.body
+    const response = await apiHelper.post('/all_kw',
+      qs.stringify({
+        token,
+        kw
+      })
+    )
+    console.log('response', response.data.result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function getVenderPurpose (req, res) {
+  try {
+    const { kw } = req.body
+    const response = await apiHelper.post('/purpose',
+      qs.stringify({
+        token,
+        kw
+      })
+    )
+    console.log('response', response.data.result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function getVenderType (req, res) {
+  try {
+    const { kw } = req.body
+    const response = await apiHelper.post('/type',
+      qs.stringify({
+        token,
+        kw
+      })
+    )
+    console.log('response', response.data.result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function getVenderDish (req, res) {
+  try {
+    const { kw } = req.body
+    const response = await apiHelper.post('/dish',
+      qs.stringify({
+        token,
+        kw
+      })
+    )
+    console.log('response', response.data.result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const restaurantController = {
-  getVenderKeyword: async (req, res) => {
-    try {
-      const { kw } = req.body
-      const response = await apiHelper.post('/all_kw',
-        qs.stringify({
-          token,
-          kw
-        })
-      )
-      console.log('response', response.data.result)
-    } catch (error) {
-      console.log(error)
-    }
-  },
   getKeyword: async (req, res) => {
     try {
       // ./keywords?kw={kw}
@@ -35,20 +81,6 @@ const restaurantController = {
       console.log(error)
     }
   },
-  getVenderPurpose: async (req, res) => {
-    try {
-      const { kw } = req.body
-      const response = await apiHelper.post('/purpose',
-        qs.stringify({
-          token,
-          kw
-        })
-      )
-      console.log('response', response.data.result)
-    } catch (error) {
-      console.log(error)
-    }
-  },
   getPurpose: async (req, res) => {
     try {
       // ./purpose?restaurant_name={restaurantName}&restaurant_id={restaurantId}
@@ -57,37 +89,9 @@ const restaurantController = {
       console.log(error)
     }
   },
-  getVenderType: async (req, res) => {
-    try {
-      const { kw } = req.body
-      const response = await apiHelper.post('/type',
-        qs.stringify({
-          token,
-          kw
-        })
-      )
-      console.log('response', response.data.result)
-    } catch (error) {
-      console.log(error)
-    }
-  },
   getType: async (req, res) => {
     try {
 
-    } catch (error) {
-      console.log(error)
-    }
-  },
-  getVenderDish: async (req, res) => {
-    try {
-      const { kw } = req.body
-      const response = await apiHelper.post('/dish',
-        qs.stringify({
-          token,
-          kw
-        })
-      )
-      console.log('response', response.data.result)
     } catch (error) {
       console.log(error)
     }
