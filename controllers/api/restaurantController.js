e74eab80d12f355479585c602cffd9ba95e1fd57
TimCheng <timazoey@gmail.com>
2021-05-26 15:40:32 +0800

    新增另外3隻 api for 前端及新增設定檔 for sequelize

--- controllers/api/restaurantController.js
+++ controllers/api/restaurantController.js
@@ -5,53 +5,128 @@ const qs = require('qs')
 const { BadRequest, NotFound } = require('../../utils/errors')
 
 const restaurantController = {
+  getDish: async (req, res, next) => {
+    try {
+      // ./dish?kw={kw}
+      const { kw } = req.query;
+      if (!kw) {
+        throw new BadRequest("Missing kw for request.");
+      }
+      const response = await apiHelper.post(
+        "/dish",
+        qs.stringify({
+          token,
+          kw,
+        })
+      );
+      if (!response.data.result.length) {
+        throw new NotFound("No match keywords for your request.");
+      }
+      return res.status(200).json({
+        status: "success",
+        result: response.data.result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  getType: async (req, res, next) => {
+    try {
+      // ./type?kw={kw}
+      const { kw } = req.query;
+      if (!kw) {
+        throw new BadRequest("Missing kw for request.");
+      }
+      const response = await apiHelper.post(
+        "/type",
+        qs.stringify({
+          token,
+          kw,
+        })
+      );
+      if (!response.data.result.length) {
+        throw new NotFound("No match keywords for your request.");
+      }
+      return res.status(200).json({
+        status: "success",
+        result: response.data.result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+  getPurpose: async (req, res, next) => {
+    try {
+      // ./purpose?kw={kw}
+      const { kw } = req.query;
+      if (!kw) {
+        throw new BadRequest("Missing kw for request.");
+      }
+      const response = await apiHelper.post(
+        "/purpose",
+        qs.stringify({
+          token,
+          kw,
+        })
+      );
+      if (!response.data.result.length) {
+        throw new NotFound("No match keywords for your request.");
+      }
+      return res.status(200).json({
+        status: "success",
+        result: response.data.result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
   getKeyword: async (req, res, next) => {
     try {
       // ./keywords?kw={kw}
-      const { kw } = req.query
+      const { kw } = req.query;
       if (!kw) {
-        throw new BadRequest('Missing kw for request.')
+        throw new BadRequest("Missing kw for request.");
       }
-      const response = await apiHelper.post('/all_kw',
+      const response = await apiHelper.post(
+        "/all_kw",
         qs.stringify({
           token,
-          kw
+          kw,
         })
-      )
+      );
       if (!response.data.result.length) {
-        throw new NotFound('No match keywords for your request.')
+        throw new NotFound("No match keywords for your request.");
       }
       return res.status(200).json({
-        status: 'success',
-        result: response.data.result
-      })
+        status: "success",
+        result: response.data.result,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   getPurpose: async (req, res) => {
     try {
       // ./purpose?restaurant_name={restaurantName}&restaurant_id={restaurantId}
-      console.log('query', req.query)
+      console.log("query", req.query);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   },
   getType: async (req, res) => {
     try {
-
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   },
   getDish: async (req, res) => {
     try {
-
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-}
+  },
+};
 
 async function getVenderKeyword (req, res) {
   try {
