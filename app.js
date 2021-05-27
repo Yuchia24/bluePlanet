cda335ad2adba929979594d8b27d1e2ff54c503c
Alice Chin <alice.chin@eztable.com>
2021-05-27 14:40:06 +0800

    feat: add morgan and log file

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const fs = require('fs')
+const morgan = require('morgan')
+const path = require('path')
 const handleErrors = require('./middleware/handleErrors')
 
 if (process.env.NODE_ENV !== 'production') {
@@ -9,9 +12,17 @@ if (process.env.NODE_ENV !== 'production') {
 
 const PORT = process.env.PORT || 3000
 
+// log setting
+const accessLogStream = fs.createWriteStream(path.join(__dirname, './logs', 'access.log'), { flags: 'a' })
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
+app.use(morgan('combined', { stream: accessLogStream, immediate: false }))
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
