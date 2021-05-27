5de4eceab55a8d8c7b2d63bcf23c144959d5a8b5
Alice Chin <alice.chin@eztable.com>
2021-05-27 15:03:57 +0800

    feat: add error log file

--- app.js
+++ app.js
@@ -14,11 +14,19 @@ const PORT = process.env.PORT || 3000
 
 // log setting
 const accessLogStream = fs.createWriteStream(path.join(__dirname, './logs', 'access.log'), { flags: 'a' })
+const errorLogStream = fs.createWriteStream(path.join(__dirname, './logs', 'error.log'), { flags: 'a' })
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 app.use(morgan('combined', { stream: accessLogStream, immediate: false }))
+app.use(morgan('combined', {
+  stream: errorLogStream,
+  immediate: false,
+  skip: (req, res) => {
+    return res.statusCode < 400
+  }
+}))
 
 app.get('/', (req, res) => {
   res.send('hello')
